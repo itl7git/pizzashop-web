4bad6eab6d74f57a7078a85502aa4b51ea4cd3fd
Italo Leal <itlcoorporativo@gmail.com>
2024-02-07 22:12:32 -0300

    Construção da página de Login

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,9 +1,9 @@
 import { Outlet } from 'react-router-dom'
 
-export function AuthLayout() {
+export function AppLayout() {
   return (
     <div>
-      <h1>Autenticação</h1>
+      <h1>Conteudo</h1>
 
       <div>
         <Outlet />
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom'
 
-import { AuthLayout } from './pages/_layouts/app'
-import { AppLayout } from './pages/_layouts/auth'
+import { AppLayout } from './pages/_layouts/app'
+import { AuthLayout } from './pages/_layouts/auth'
 import { Dashboard } from './pages/app/dashboard'
 import { SignIn } from './pages/auth/sign-in'
 
