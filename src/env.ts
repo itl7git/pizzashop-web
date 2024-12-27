be838fae6e4dee3ca5a5034845fc46c85f50ad9d
Italo Leal <itlcoorporativo@gmail.com>
2024-12-27 16:52:54 -0300

    feat: Obtem os dados do Perfil no cabeçalho com Loading State

--- src/env.ts
+++ src/env.ts
@@ -2,6 +2,7 @@ import { z } from 'zod'
 
 const envShema = z.object({
   VITE_API_URL: z.string().url(),
+  VITE_ENABLED_API_DELAY: z.string().transform((value) => value === 'true'),
 })
 
 export const env = envShema.parse(import.meta.env)
